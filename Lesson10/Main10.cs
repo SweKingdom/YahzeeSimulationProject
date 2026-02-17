d9c654fe3b7b20e493be4b099d5629b3bbf96c4f
Martin <martin_lenart@icloud.com>
2026-02-17 12:24:40 +0100

    update

---
+++ Lesson10/Main10.cs
@@ -0,0 +1,11 @@
+namespace Playground.Lesson10;
+public static class Main10
+{
+    public static void Entry(string[] args = null)
+    {
+        System.Console.WriteLine("Hello Lesson 10!");
+        Examples.AggregateExamples1.RunExamples();
+        //Examples.AggregateExamples2.RunExamples();
+    }
+}
+    
